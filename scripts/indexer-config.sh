469de7594dd5ed1c8c309136d9e41ba75458dad7
abstrct <abstrct@slow.ninja>
2025-08-01 13:28:16 -0400

    Basic indexer configuration prep

---
+++ scripts/indexer-config.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "Configuring node for use as a postgres indexer"
+
+if ! [ -f $STRUCTS_PATH/status/network ]; then
+  echo "Network needs to be configured first"
+  exit 1
+fi
+
+# TODO Check that postgres works
+
+# TODO Deploy a testnet specific schema?
+
+echo "Moving Indexer specific configuration files into place"
+cp ~/config/indexer/* $STRUCTS_PATH/config/
+
+echo "Updating config.toml to point to postgres"
+echo $STRUCTS_INDEXER_PG_CONNECTION
+sed -i 's#psql-conn.*.$#psql-conn = "'$STRUCTS_INDEXER_PG_CONNECTION'"#' /var/structs/chain/config/config.toml
+
+echo "Node successfully configured for indexing"
+
+exit 0
