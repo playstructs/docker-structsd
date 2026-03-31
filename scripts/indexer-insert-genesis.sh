d6b49d779c57ff9a5455a2913da4a57f4e0e663a
abstrct <abstrct@slow.ninja>
2026-03-31 16:47:37 -0400

    insert genesis expanded to properly deal with additional genesis data such as delegations and ore.

--- scripts/indexer-insert-genesis.sh
+++ scripts/indexer-insert-genesis.sh
@@ -2,44 +2,114 @@
 
 echo "Inserting Genesis Data into Structs DB"
 
-echo "Deleting old stuff"
+echo "Deleting old genesis entries"
 psql ${STRUCTS_INDEXER_PG_CONNECTION} --set=sslmode=require -c "DELETE FROM structs.ledger WHERE action = 'genesis';"
 
-ALL_GENESIS_BLOB=$(cat $STRUCTS_PATH/config/genesis.json)
+GENESIS_FILE="$STRUCTS_PATH/config/genesis.json"
 
-ALL_GENESIS_COUNT=`echo ${ALL_GENESIS_BLOB} | jq ".app_state.bank.balances" | jq length `
-echo "Found ${ALL_GENESIS_COUNT} Genesis records"
+ALL_GENESIS_TIME=$(jq -r '.genesis_time' "$GENESIS_FILE")
+echo "Genesis time: ${ALL_GENESIS_TIME}"
 
-ALL_GENESIS_TIME=`echo ${ALL_GENESIS_BLOB} | jq -r ".genesis_time" `
+SQL="BEGIN;\n"
 
-for (( p=0; p<ALL_GENESIS_COUNT; p++ ))
-do
-  GENESIS_ADDRESS=`echo ${ALL_GENESIS_BLOB} | jq -r ".app_state.bank.balances[${p}].address"`
+# ---------------------------------------------------------------------------
+# 1. Bank Balances
+# ---------------------------------------------------------------------------
+echo "Processing bank balances..."
 
-  GENESIS_COUNT=`echo ${ALL_GENESIS_BLOB} | jq -r ".app_state.bank.balances[${p}].coins" | jq length `
+BANK_SQL=$(jq -r --arg gt "$ALL_GENESIS_TIME" '
+  .app_state.bank.balances[] |
+  .address as $addr |
+  .coins[] |
+  "INSERT INTO structs.ledger(address, amount_p, block_height, time, action, direction, denom) VALUES(\u0027" + $addr + "\u0027,\u0027" + .amount + "\u0027, 0, \u0027" + $gt + "\u0027, \u0027genesis\u0027, \u0027credit\u0027, \u0027" + .denom + "\u0027);"
+' "$GENESIS_FILE")
 
-  for (( c=0; c<GENESIS_COUNT; c++ ))
-  do
+SQL+="${BANK_SQL}\n"
 
-    GENESIS_COIN_DENOM=`echo ${ALL_GENESIS_BLOB} | jq -r ".app_state.bank.balances[${p}].coins[${c}].denom"`
-    GENESIS_COIN_AMOUNT=`echo ${ALL_GENESIS_BLOB} | jq -r ".app_state.bank.balances[${p}].coins[${c}].amount"`
+BANK_COUNT=$(echo "$BANK_SQL" | grep -c "^INSERT")
+echo "  ${BANK_COUNT} bank balance entries"
 
-    QUERY="INSERT INTO structs.ledger(address, amount_p, block_height, time, action, direction, denom) VALUES('${GENESIS_ADDRESS}','${GENESIS_COIN_AMOUNT}', 0, '${ALL_GENESIS_TIME}', 'genesis', 'credit', '${GENESIS_COIN_DENOM}'); "
+# ---------------------------------------------------------------------------
+# 2. Staking Delegations
+# ---------------------------------------------------------------------------
+echo "Processing staking delegations..."
 
-    echo "${QUERY}"
+DELEGATION_SQL=$(jq -r --arg gt "$ALL_GENESIS_TIME" '
+  (.app_state.staking.validators | map({key: .operator_address, value: {tokens: (.tokens | tonumber), shares: (.delegator_shares | tonumber)}}) | from_entries) as $validators |
 
-    psql ${STRUCTS_INDEXER_PG_CONNECTION} --set=sslmode=require -c "${QUERY}"
-  done
-done
+  .app_state.staking.delegations[] |
+  .delegator_address as $delegator |
+  .validator_address as $validator |
+  (.shares | tonumber) as $shares |
 
+  ($validators[$validator].tokens // 0) as $vtokens |
+  ($validators[$validator].shares // 1) as $vshares |
+  (if $vshares > 0 then (($shares * $vtokens / $vshares) | floor | tostring) else "0" end) as $amount |
 
-# Yeah, it's stupid, but it'll work for now. TODO Fix to be generic or fix on new genesis
-QUERY="INSERT INTO structs.ledger(address, counterparty, amount_p, block_height, time, action, direction, denom) VALUES('structs1ul8sd7nk573aw2gyzzwn2ahxqzrq0qg70en5e9','structsvaloper1ul8sd7nk573aw2gyzzwn2ahxqzrq0qg7f8g7t2','300000000', 0, '${ALL_GENESIS_TIME}', 'genesis', 'debit', 'ualpha'); "
-echo "${QUERY}"
-psql ${STRUCTS_INDEXER_PG_CONNECTION} --set=sslmode=require -c "${QUERY}"
-QUERY="INSERT INTO structs.ledger(address, counterparty, amount_p, block_height, time, action, direction, denom) VALUES('structs1ul8sd7nk573aw2gyzzwn2ahxqzrq0qg70en5e9','structsvaloper1ul8sd7nk573aw2gyzzwn2ahxqzrq0qg7f8g7t2','300000000', 0, '${ALL_GENESIS_TIME}', 'genesis', 'credit', 'ualpha.infused'); "
-echo "${QUERY}"
-psql ${STRUCTS_INDEXER_PG_CONNECTION} --set=sslmode=require -c "${QUERY}"
-QUERY="INSERT INTO structs.ledger(counterparty, address, amount_p, block_height, time, action, direction, denom) VALUES('structs1ul8sd7nk573aw2gyzzwn2ahxqzrq0qg70en5e9','structsvaloper1ul8sd7nk573aw2gyzzwn2ahxqzrq0qg7f8g7t2','300000000', 0, '${ALL_GENESIS_TIME}', 'genesis', 'credit', 'ualpha.infused'); "
-echo "${QUERY}"
-psql ${STRUCTS_INDEXER_PG_CONNECTION} --set=sslmode=require -c "${QUERY}"
+  "INSERT INTO structs.ledger(address, counterparty, amount_p, block_height, time, action, direction, denom) VALUES(\u0027" + $delegator + "\u0027, \u0027" + $validator + "\u0027, \u0027" + $amount + "\u0027, 0, \u0027" + $gt + "\u0027, \u0027genesis\u0027, \u0027credit\u0027, \u0027ualpha.infused\u0027);",
+
+  "INSERT INTO structs.ledger(address, counterparty, amount_p, block_height, time, action, direction, denom) VALUES(\u0027" + $validator + "\u0027, \u0027" + $delegator + "\u0027, \u0027" + $amount + "\u0027, 0, \u0027" + $gt + "\u0027, \u0027genesis\u0027, \u0027credit\u0027, \u0027ualpha.infused\u0027);"
+' "$GENESIS_FILE")
+
+SQL+="${DELEGATION_SQL}\n"
+
+DELEGATION_COUNT=$(echo "$DELEGATION_SQL" | grep -c "^INSERT")
+echo "  ${DELEGATION_COUNT} delegation entries (from $(( DELEGATION_COUNT / 2 )) delegations)"
+
+# ---------------------------------------------------------------------------
+# 3. Unbonding Delegations
+# ---------------------------------------------------------------------------
+echo "Processing unbonding delegations..."
+
+UNBONDING_SQL=$(jq -r --arg gt "$ALL_GENESIS_TIME" '
+  .app_state.staking.unbonding_delegations[] |
+  .delegator_address as $delegator |
+  .validator_address as $validator |
+  .entries[] |
+  .balance as $amount |
+
+  "INSERT INTO structs.ledger(address, counterparty, amount_p, block_height, time, action, direction, denom) VALUES(\u0027" + $delegator + "\u0027, \u0027" + $validator + "\u0027, \u0027" + $amount + "\u0027, 0, \u0027" + $gt + "\u0027, \u0027genesis\u0027, \u0027credit\u0027, \u0027ualpha.defusing\u0027);",
+
+  "INSERT INTO structs.ledger(address, counterparty, amount_p, block_height, time, action, direction, denom) VALUES(\u0027" + $validator + "\u0027, \u0027" + $delegator + "\u0027, \u0027" + $amount + "\u0027, 0, \u0027" + $gt + "\u0027, \u0027genesis\u0027, \u0027credit\u0027, \u0027ualpha.defusing\u0027);"
+' "$GENESIS_FILE")
+
+SQL+="${UNBONDING_SQL}\n"
+
+UNBONDING_COUNT=$(echo "$UNBONDING_SQL" | grep -c "^INSERT")
+echo "  ${UNBONDING_COUNT} unbonding entries (from $(( UNBONDING_COUNT / 2 )) unbonding delegations)"
+
+# ---------------------------------------------------------------------------
+# 4. Player Ore Balances (from gridList, attribute type 0 on object type 1)
+# ---------------------------------------------------------------------------
+echo "Processing player ore balances..."
+
+ORE_SQL=$(jq -r --arg gt "$ALL_GENESIS_TIME" '
+  (.app_state.structs.playerList | map({key: .index, value: .primaryAddress}) | from_entries) as $players |
+
+  [.app_state.structs.gridList[] | select(.attributeId | startswith("0-1-")) | select(.value != "0")] |
+  .[] |
+  (.attributeId | split("-") | .[2]) as $playerIndex |
+  ($players[$playerIndex] // "") as $addr |
+
+  select($addr != "") |
+
+  "INSERT INTO structs.ledger(address, amount_p, block_height, time, action, direction, denom) VALUES(\u0027" + $addr + "\u0027, \u0027" + .value + "\u0027, 0, \u0027" + $gt + "\u0027, \u0027genesis\u0027, \u0027credit\u0027, \u0027ore\u0027);"
+' "$GENESIS_FILE")
+
+SQL+="${ORE_SQL}\n"
+
+ORE_COUNT=$(echo "$ORE_SQL" | grep -c "^INSERT")
+echo "  ${ORE_COUNT} player ore entries"
+
+# ---------------------------------------------------------------------------
+# Execute all SQL in a single transaction
+# ---------------------------------------------------------------------------
+SQL+="COMMIT;\n"
+
+TOTAL_COUNT=$(echo -e "$SQL" | grep -c "^INSERT")
+echo ""
+echo "Executing ${TOTAL_COUNT} total INSERT statements in a single transaction..."
+
+echo -e "$SQL" | psql ${STRUCTS_INDEXER_PG_CONNECTION} --set=sslmode=require
+
+echo "Genesis insert complete!"
